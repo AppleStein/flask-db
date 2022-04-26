69934520c1d12e6d4d05ba4ba29f72b54de64354
AppleStein <pianonono1129@gmail.com>
2022-04-26 21:49:03 +0900

    input

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import hashlib
 import datetime
+from sqlite3 import Timestamp
 from flask import *
 import string
 from sqlalchemy import DATETIME, create_engine, Column, String, Integer
@@ -20,12 +21,12 @@ class User(Base):
     __tablename__ = 'users'
     #ユーザとパスワードを自動生成
     name = Column(String, primary_key = True, unique = True)
-    pssw = Column(String)
+    passw = Column(String)
 
     def __repr__(self):
-        return "User<{}>".format(self.name)
+        return "User<{}, {}, {}>".format(self.name)
 
-
+#投稿
 class Content(Base):
     __tablename__ = 'contents'
     id = Column(Integer, primary_key=True)
@@ -42,6 +43,30 @@ Base.metadata.create_all(engine)
 #pythonとdbの経路->作成
 SessionMaker = sessionmaker(bind=engine)
 session = scoped_session(SessionMaker)
+
+
+@app.route("/", methods=["GET", "POST"])
+def main_page():
+
+    cont = session.query(Content).all()
+    if request.method == "GET":
+        return render_template("mainpage.html", cont=cont)
+
+    user = session.query(User).get(request.form["name"].strip)
+
+    if user:
+        if user.passw != str(hashlib.sha256(request.form["pass"].strip().encode("utf-8")).digest()):
+            return render_template("mainpage.html", cont=cont)
+        else:
+            user = User(name = request.form["name"], passw = str(hashlib.sha256(request.form["pass"].strip().encode("utf-8").digest())))
+            session.add(user)
+
+    mess = Content(name = request.form["name"], content = request.form["content"], timestamp = datetime.datetime.now())
+    session.add(mess)
+    session.commit()
+    cont = session.query(Content).all()
+    return render_template("mainpage.html", cont = cont)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port = 8888, threaded=True)
